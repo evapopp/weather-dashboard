02e829754e197130003af0c56ad293f8925b490a
eva <83175995+evapopp@users.noreply.github.com>
2021-07-14 20:30:15 -0700

    added 5 day forecast

--- script.js
+++ script.js
@@ -4,8 +4,36 @@ const windEl = document.getElementById("wind");
 const humidityEl = document.getElementById("humidity");
 const uvEl = document.getElementById("uv");
 const pastSearchEl = document.getElementById('past-search')
+const dailyTempArr = []
+const dailyWindArr = []
+const dailyHumidArr = []
+
 const today = new Date();
-var date = (today.getMonth()+1) + '/' + today.getDate() + '/' + today.getFullYear();
+const date = (today.getMonth()+1) + '/' + today.getDate() + '/' + today.getFullYear();
+
+const day1 = new Date(today);
+day1.setDate(day1.getDate() + 1);
+const date1 =(day1.getMonth()+1) + '/' + day1.getDate() + '/' + day1.getFullYear();
+
+const day2 = new Date(today);
+day2.setDate(day2.getDate() + 2);
+const date2 =(day2.getMonth()+1) + '/' + day2.getDate() + '/' + day2.getFullYear();
+
+const day3 = new Date(today)
+day3.setDate(day3.getDate() + 3)
+const date3 =(day3.getMonth()+1) + '/' + day3.getDate() + '/' + day3.getFullYear();
+
+const day4 = new Date(today)
+day4.setDate(day4.getDate() + 4)
+const date4 =(day4.getMonth()+1) + '/' + day4.getDate() + '/' + day4.getFullYear();
+
+const day5 = new Date(today)
+day5.setDate(day5.getDate() + 5)
+const date5 =(day5.getMonth()+1) + '/' + day5.getDate() + '/' + day5.getFullYear();
+
+const dateArr = [date1, date2, date3, date4, date5];
+
+
 console.log(date)
 
 renderUserQuery();
@@ -48,23 +76,42 @@ function handleData(data){
     tempEl.textContent = "Temp: " + temp + " degrees F";
     windEl.textContent = "Wind: " + wind + " MPH";
     humidityEl.textContent = "Humidity: " + humidity + "%";
-    console.log(wind)
 }
 
 function getWeatherByCoords(coords){
     const lat = coords.coord.lat;
     const lon = coords.coord.lon;
-    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=d91f911bcf2c0f925fb6535547a5ddc9`)
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=d91f911bcf2c0f925fb6535547a5ddc9&&units=imperial`)
     .then(function(response){
         return response.json();
     })
     .then(handleForecast);
 }
 
+
+
+
 function handleForecast(data){
     console.log(data)
     const uv = data.current.uvi;
     uvEl.textContent = 'UV Intex: ' + uv;
+    for(i = 0; i < 5; i++){
+        const cardHeaderEl = document.getElementById(`${i}`);
+        const dailyTempEl = document.getElementById(`temp${i}`);
+        const dailyWindEl = document.getElementById(`wind${i}`);
+        const dailyHumidEl = document.getElementById(`humid${i}`);
+        const dailyTemp = data.daily[i].temp.day;
+        const dailyWind = data.daily[i].wind_speed;
+        const dailyHumid = data.daily[i].humidity;
+        dailyTempArr.push(dailyTemp);
+        dailyWindArr.push(dailyWind);
+        dailyHumidArr.push(dailyHumid);
+        cardHeaderEl.textContent = dateArr[i];
+        dailyTempEl.textContent = `Temperature: ${dailyTempArr[i]} degrees`;
+        dailyWindEl.textContent = `Wind Speed: ${dailyWindArr[i]} MPH`;
+        dailyHumidEl.textContent = `Humidity: ${dailyHumidArr[i]}%`;
+    }
+
 }
 
 
